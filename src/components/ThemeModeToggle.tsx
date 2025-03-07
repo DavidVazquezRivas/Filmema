f355dfdbd75ac580e4c6869250df9be798122d33
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-03-07 16:25:29 +0100

    accesibility fix

--- src/components/ThemeModeToggle.tsx
+++ src/components/ThemeModeToggle.tsx
@@ -2,13 +2,18 @@ import { IconButton } from '@mui/material'
 import { useThemeContext } from '@/theme/ThemeContextProvider'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeIcon from '@mui/icons-material/DarkMode'
+import { useTranslation } from 'react-i18next'
 
 export const ThemeModeToggle = () => {
   const { mode, toggleColorMode } = useThemeContext()
+  const { t } = useTranslation()
 
+  const label =
+    mode === 'dark' ? t('header.theme.lightLabel') : t('header.theme.darkLabel')
+  const icon = mode === 'dark' ? <LightModeIcon /> : <DarkModeIcon />
   return (
-    <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
-      {mode === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
+    <IconButton onClick={toggleColorMode} color="inherit" aria-label={label}>
+      {icon}
     </IconButton>
   )
 }
