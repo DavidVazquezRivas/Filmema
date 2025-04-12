5b1134733f38155748b51d9f8e8998da5875e919
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-04-12 03:34:52 +0200

    Add lite-youtube dependency
    Add lite-youtube dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import '@/styled-components/global.css'
+import '@justinribeiro/lite-youtube'
+import '@/translation/i18n'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@/App.tsx'
 import { ThemeContextProvider } from '@/theme/ThemeContextProvider.tsx'
-import '@/translation/i18n'
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
 
